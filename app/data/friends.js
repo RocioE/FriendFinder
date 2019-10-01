c2cb3fa2682b595859ab9fa9eb320327b88ebca2
RocioE <48306304+RocioE@users.noreply.github.com>
2019-10-01 10:54:15 -0700

    updated differences

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,5 @@
 var friendsArray = 
+[
 {
     "name":"Mia Wallace",
     "photo":"https://i.pinimg.com/originals/a9/01/29/a9012982eea651ee31e21f4ad11fe96b.jpg",
@@ -16,33 +17,38 @@ var friendsArray =
 
         
       ]
-    }
-
+    },
+    {
+      "name":"Thee Girl",
+   "photo":"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+  "scores":[
+    3, 
+    2, 
+    6, 
+    4, 
+    5, 
+    1, 
+    2, 
+    5, 
+    4, 
+    1
+                ]
+  },
+  {
+    "name":"Number One",
+   "photo":"https://i.pinimg.com/564x/dd/31/d2/dd31d2dd156204fcf9b6aa1c54455adb.jpg",
+   "scores":[
+     3,
+     5,
+     2,
+     4,
+     1,
+     5,
+     2,
+     1,
+     3
+      ]
+ },
+  ];
 
 module.exports = friendsArray;
-
-//   {
-//     "name":"Mia Wallace",
-//     "photo":"hhttps://s-media-cache-ak0.pinimg.com/originals/a9/01/29/a9012982eea651ee31e21f4ad11fe96b.jpg",
-//     "scores":[
-        
-//       ]
-//   },
-
-// {
-//     "name":"Thee Girl",
-//     "photo":"https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-//     "scores":[
-        
-//       ]
-//   },
-
-//   {
-//     "name":"Number One",
-//     "photo":"https://i.pinimg.com/564x/dd/31/d2/dd31d2dd156204fcf9b6aa1c54455adb.jpg",
-//     "scores":[
-        
-//       ]
-//   },
-
-  
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,12 +1,33 @@
 var people = require("../data/friends");
 
 
+function computeDifference(scoresA, scoresB) {
+    var differences = 0
+
+    differences = differences + Math.abs(scoresA[0] - scoresB[0])
+    differences = differences + Math.abs(scoresA[1] - scoresB[1])
+    differences = differences + Math.abs(scoresA[2] - scoresB[2])
+    differences = differences + Math.abs(scoresA[3] - scoresB[3])
+    differences = differences + Math.abs(scoresA[4] - scoresB[4])
+    differences = differences + Math.abs(scoresA[5] - scoresB[5])
+    differences = differences + Math.abs(scoresA[6] - scoresB[6])
+    differences = differences + Math.abs(scoresA[7] - scoresB[7])
+    differences = differences + Math.abs(scoresA[8] - scoresB[8])
+    differences = differences + Math.abs(scoresA[9] - scoresB[9])
+    return differences
+}
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
-        res.json(people);
+        res.json(people[1]);
     });
     app.post("/api/friends", function(req, res) {
-        res.json(people);
+        var userScores= req.body.scores
+        var miaScores= people[0].scores
+        var miaDifference = computeDifference(userScores, miaScores)    
+        var theeGirlScores=people[1].scores
+        var theeGirlDifference = computeDifference(userScores, theeGirlScores)
+    
+        res.json(people[1]);
     });
   };
